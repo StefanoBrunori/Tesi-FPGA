a2630059167d9cb04ad600c4bc21a982005fac6e
StefanoBrunori <80215428+StefanoBrunori@users.noreply.github.com>
2021-05-06 14:13:52 +0200

    Update FSM.v

--- Mining/FSM.v
+++ Mining/FSM.v
@@ -7,8 +7,7 @@ module FSM(
     input clk,
     input reset,
     
-    output reg we,
-    output reg re,
+    output reg we,   
     
     input fine_scrittura,
     input fine_lettura,
@@ -22,21 +21,18 @@ module FSM(
     always@(posedge clk) begin
         if (fine) begin
             state <= 2'b00;
-            we <= 0;
-            re <= 0;
+            we <= 0;          
         end
         if (reset) begin
             indirizzo_read <= 9'h0;            
             state <= 2'b00;
-            we <= 1;
-            re <= 0;
+            we <= 1;           
         end
         
         else begin        
             if (start) begin
                 state <= 2'b01;
-                we <= 1;
-                re <= 0;                       
+                we <= 1;                                      
             end
             
             if (fine_scrittura) begin
@@ -45,15 +41,13 @@ module FSM(
                 end
                 else begin
                     state <= 2'b10;
-                    we <= 0;
-                    re <= 1;
+                    we <= 0;                   
                     indirizzo_read <= 10'h0;
                 end
             end           
             
             if (fine_lettura) begin
-                state <= 2'b11;
-                re <= 0;
+                state <= 2'b11;                
             end                
         end                  
     end    
