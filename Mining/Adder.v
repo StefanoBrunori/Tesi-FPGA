7735d5a3424524ac6d616f7708d6217b31391553
StefanoBrunori <80215428+StefanoBrunori@users.noreply.github.com>
2021-05-06 14:12:37 +0200

    Update Adder.v

--- Mining/Adder.v
+++ Mining/Adder.v
@@ -1,5 +1,6 @@
 `timescale 1ns / 1ps
 
+//SHA-256 output for "": e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
 
 module Adder(
     input wire [7:0] in,
@@ -12,7 +13,7 @@ module Adder(
     output reg fine,
     output reg [12:0] out
     );
-    
+       
     
     //Inizio della Funzione: "Somma dei bit del messaggio"
     
@@ -22,7 +23,7 @@ module Adder(
     reg [12:0] adder;
          
        
-    // Controllo lo stato della FSM: se sta a 10 inizio la somma, se sta a 11 mando in output il risultato
+    // Controllo lo stato della FSM: se sta a 01 inizio la somma, se sta a 10 mando in output il risultato
     always@(posedge clk)
         if (reset) begin 
             i = 0;      
@@ -35,11 +36,19 @@ module Adder(
             case(state)
                 
                 //Sommo gli 8 bit presi in input
+                2'b00 : begin 
+                end
+                
+                //Libero il registro di appoggio "adder"
+                2'b01 : begin
+                    adder <= 8'h0;
+                end 
+                
+                //Eseguo la somma degli 8 bit
                 2'b10 : begin                     
                     for (i=0;i<=7;i=i+1) begin
                         adder = adder+in[i];                                            
-                    end
-                                                                                                               
+                    end                                                                                                               
                 end
                     
                 //Mando in output il risultato        
