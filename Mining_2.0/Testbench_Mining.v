f82797dac34320bbf94922dcff8d99ae79f89753
StefanoBrunori <80215428+StefanoBrunori@users.noreply.github.com>
2021-06-03 10:45:56 +0200

    Update Testbench_Mining.v

--- Mining_2.0/Testbench_Mining.v
+++ Mining_2.0/Testbench_Mining.v
@@ -2,7 +2,8 @@
 
 module Mining_test;
     
-    parameter LENGHT = 1;
+    //Lunghezza del messaggio
+    parameter LENGHT = 1000;
     
     reg clock = 0; 
     reg reset = 0;
@@ -13,10 +14,14 @@ module Mining_test;
     reg stopw = 0;
     wire [2:0] state;
     reg [31:0] message;
-    reg [14:0] mess_lenght = LENGHT; 
+    reg [63:0] mess_lenght = LENGHT; 
     wire [511:0] chunk;
     
-    wire [255:0] HASH;
+    wire fine_mining;
+    wire [31:0] NONCE;
+    wire reset_fsm;       
+    
+    wire [63:0] OUT;
     
     reg [31:0] h0 = 32'h6a09e667;
     reg [31:0] h1 = 32'hbb67ae85;
@@ -26,21 +31,28 @@ module Mining_test;
     reg [31:0] h5 = 32'h9b05688c;
     reg [31:0] h6 = 32'h1f83d9ab;
     reg [31:0] h7 = 32'h5be0cd19;
+       
+    reg [29999:0] messaggio;
     
-    //La prova l'ho eseguita con input:= "0"
-    reg [29999:0] messaggio = 1'b0;
+    integer i;
+    initial begin
+        for (i=0;i<LENGHT;i=i+1) begin
+            messaggio[i] = {$random}%2;
+        end
+    end  
     
-    integer j = 0;
+    integer j = LENGHT;
     integer k = 0;
     always@(posedge clock) begin
         if (state == 3'b001) begin
-            if (j>LENGHT/32) begin
+            if (j<31) begin
+                message = messaggio[(LENGHT%32)-1:0];
                 start <= 0;
                 stopw <= 1;          
             end
             else begin
-                message = messaggio[(j*32)+:32];                    
-                j = j + 1;
+                message = messaggio[(j-1)-:32];                   
+                j = j - 32;
                 k = k + 32;
                 if (k%512 == 0) begin
                    indirizzo = indirizzo + 1; 
@@ -54,17 +66,25 @@ module Mining_test;
         .reset(reset),
         .start(start),
         .stopw(stopw),       
-        .fine(fine),                       
-        .state(state)
+        .fine(fine),
+        .fine_mining(fine_mining),
+                               
+        .state(state),
+        .OUT(OUT),
+        .reset_fsm(reset_fsm),
+        .NONCE(NONCE)
         );
     
     Preprocessing p1(
         .clock(clock),
         .reset(reset),
+        .reset_fsm(reset_fsm),
         .indirizzo(indirizzo),
         .state(state),
         .message(message),
         .mess_lenght(mess_lenght),
+        .NONCE(NONCE),
+        
         .chunk(chunk),
         .fine(fine)        
         );
@@ -72,10 +92,11 @@ module Mining_test;
     Chunks c1(
         .clock(clock),
         .reset(reset),
+        .reset_fsm(reset_fsm),
         .state(state),
         .chunk(chunk),
-                                        
-        .HASH(HASH)
+                                          
+        .fine_mining(fine_mining)
         );
     
     always #5 clock = ~clock;  
