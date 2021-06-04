b5663e368aa41c9429581f062842126d69518f7d
StefanoBrunori <80215428+StefanoBrunori@users.noreply.github.com>
2021-06-04 15:23:17 +0200

    Update Mining_FSM.v

--- Mining_2.0/Mining_FSM.v
+++ Mining_2.0/Mining_FSM.v
@@ -5,72 +5,53 @@ module Mining_FSM(
     input reset,
     input start,
     input stopw,    
-    input fine,
-    input fine_mining,
+    input fine,    
+    input wire [255:0] HASH,
         
     output reg [2:0] state,
-    output reg [63:0] OUT,
-    output reg reset_fsm,
-    output reg [31:0] NONCE,
-    output reg nonce_flag 
+    output reg [63:0] OUT     
     );
     
     always@(posedge clock) begin
-        if (^state === 1'bx) state <= 3'b000;       
+        if (^state === 1'bx) state <= 3'h0;       
         
         //reset
         if (reset) begin
-            state <= 3'b000;
-            NONCE <= 32'h0;
-            nonce_flag <= 1'b0;
+            state <= 3'h0;           
         end
         
         case (state) 
-            3'b000: if (start) begin
-                        OUT <= "Niente!";
-                        state <= 3'b001;
-                        NONCE <= 32'h0;
-                        nonce_flag <= 1'b0;
-                    end
+            3'h0: begin
+                    OUT <= "Niente!";
+                    state <= 3'h1;                                              
+                  end        
                     
-            3'b001: if (stopw) begin
-                        state <= 3'b010;
-                    end
+            3'h1: if (stopw) begin
+                     state <= 3'h2;
+                  end
                     
-            3'b010: begin
-                        if (fine_mining) begin
-                            OUT <= "Trovato!";
-                            state <= 3'b111;                               
-                        end
-                        state <= 3'b011;
-                        reset_fsm <= 1'b0;
-                    end
-                    
-            3'b011: begin                                                                 
-                        if (fine) begin
-                            state <= 3'b111;
-                        end
-                        else state <= 3'b100;                                              
-                    end
-                    
-            3'b100: state <= 3'b101;
+            3'h2: begin                  
+                    state <= 3'h3;                  
+                  end
             
-            3'b101: state <= 3'b110;
+            3'h3: state <= 3'h4;
             
-            3'b110: state <= 3'b011;
-                                       
-            3'b111: begin
-                        if (fine_mining) begin
-                            OUT <= "Trovato!";                               
-                        end
-                        else begin
-                            OUT <= "Niente!";
-                            state <= 3'b010;
-                            NONCE <= NONCE + 1;
-                            nonce_flag <= 1'b1;
-                            reset_fsm <= 1'b0;                           
-                        end                        
-                    end 
+            3'h4: state <= 3'h5;
+            
+            3'h5: if (fine) state <= 3'h6;
+                  else state <= 3'h3;
+                    
+            3'h6: state <= 3'h7;
+                  
+            3'h7: begin
+                if (HASH[255-:10] == 10'h0) begin
+                     OUT <= "Trovato!";     
+                end
+                else begin
+                    state <= 3'h2;
+                end  
+            end
+            
         endcase
     end
     
