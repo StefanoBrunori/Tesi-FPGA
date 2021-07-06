199727b30ae28688cde95d6f18cd72b91df23b79
StefanoBrunori <80215428+StefanoBrunori@users.noreply.github.com>
2021-07-06 12:42:53 +0200

    Update Mining_FSM.v

--- Mining_3.0/Mining_FSM.v
+++ Mining_3.0/Mining_FSM.v
@@ -12,6 +12,7 @@ module Mining_FSM(
     input [31:0] message,
     input [511:0] bram_data_out,
     
+    output reg flag,    
     output reg [511:0] chunk,
     output reg [31:0] bram_data_in,
     output reg cs_n,
@@ -20,43 +21,47 @@ module Mining_FSM(
     output reg [15:0] addr,
     output reg [8:0] addr_width,
     output reg [2:0] state,
-    output reg OUT,
-    output reg [31:0] NONCE_OUT
+    output reg [31:0] NONCE_OUT    
     );
     
     reg [15:0] index;
-    reg fine;  
-    reg flag;
-    reg [31:0] nonce_attuale;  
+    reg fine;     
+    reg [31:0] nonce_attuale; 
+    reg [5:0] indice; 
          
     always@(posedge clock) begin
         if (^state === 1'bx) state <= 3'h0;
-        if (^wr_n === 1'bx) wr_n = 1'b1;
+        if (^wr_n === 1'bx) wr_n = 1'b0;
         if (^rd_n === 1'bx) rd_n = 1'b1;
-        if (^cs_n === 1'bx) cs_n = 1'b1;
+        if (^cs_n === 1'bx) cs_n = 1'b0;
         if (^index === 1'bx) index = 1'b0;
         if (^fine === 1'bx) fine = 1'b0;       
         if (^bram_data_in === 1'bx) bram_data_in = 32'h0;
         if (^addr === 1'bx) addr = 16'h0;
-        if (^addr_width === 1'bx) addr_width = 9'h0;
-        if (^OUT === 1'bx) OUT = 1'b0;      
+        if (^addr_width === 1'bx) addr_width = 9'h0;   
+        if (^flag === 1'bx) flag = 1'b0;                 
         
         //reset
         if (~reset) begin
             state <= 3'h0;           
         end              
+        else begin               
         
-        case (state) 
+        case (state)                    
+                                
             3'h0: begin
-                    OUT <= 0;
-                    state <= 3'h1;                                                             
-                  end        
-                    
-            3'h1: begin
-                    if (stopw) begin                                            
-                        wr_n = 1'b1;
-                        rd_n = 1'b0;
-                        state <= 3'h2;                                             
+                    if (stopw) begin
+                        if (~flag) begin                                            
+                            wr_n = 1'b1;
+                            rd_n = 1'b0;                           
+                            addr = indirizzo_nonce;
+                            addr_width = nonce_width;
+                            flag = 1;
+                        end
+                        else begin
+                            state <= 3'h1;
+                            flag = 0;
+                        end                                             
                     end
                     else begin  
                         addr = indirizzo;
@@ -64,85 +69,93 @@ module Mining_FSM(
                         bram_data_in = message;                     
                         cs_n = 1'b0;
                         wr_n = 1'b0;                  
-                    end                    
+                    end
+                     
                   end 
                                                          
-            3'h2: begin
+            3'h1: begin
                     if (~flag) begin
                         addr = indirizzo_nonce;
                         addr_width = nonce_width;
-                        bram_data_in = bram_data_out[nonce_width-:32] + 1;                       
-                        flag = 1;                                             
-                    end
-                    else begin
-                        state <= 3'h3;
-                        flag = 0;
+                        bram_data_in = bram_data_out[nonce_width-:32] + 1;                                              
+                        flag = 1; 
                         rd_n = 1'b1;
-                        wr_n = 1'b0;
+                        wr_n = 1'b0;                                            
+                    end
+                    else begin                                               
+                        state <= 3'h2;
+                        flag = 0;
+                        rd_n = 1'b1;                       
+                        addr = index;                                              
                     end        
                   end
                                                                  
-            3'h3: begin 
-                    addr = index;                  
-                    rd_n = 1'b0;
-                    chunk = bram_data_out;                                                                                                
+            3'h2: begin 
+                    addr = index;                                     
+                    chunk = bram_data_out;                                                                                                                  
                     index = index + 1;
-                    //Index ha raggiunto l'ultimo blocco
+                    state <= 3'h3;
+                    
                     if (index == indirizzo) begin                                                                                            
                         fine = 1'b1;
                         index = 16'h0;
                     end
                     rd_n = 1'b1;
                     wr_n = 1'b1;
-                    state <= 3'h4;
                   end
             
-            3'h4: begin
+            3'h3: begin                   
                     if (~flag) begin
-                        indice = 0;
+                        indice = 6'd16;
                         flag = 1;
                     end                    
                     else begin
-                        state <= 3'h5;
-                        flag = 0;                                                    
+                        if (indice == 6'h0) begin
+                            state <= 3'h4;
+                            flag = 1'b0;                          
+                        end
+                        else begin
+                            indice = indice + 1;                           
+                        end                                                    
                     end
-                  end                                                    
+                  end
                      
-            3'h5: begin
-                    if (~flag) flag = 1;
-                    else begin 
-                        flag = 0;
-                        if (indice == 6'd63) begin                  
+            3'h4: begin
+                    if (flag) begin
+                        if (indice == 6'h0) begin                  
                             addr = index;
                             rd_n = 1'b0;
                             if (fine) begin
-                                state <= 3'h6;
+                                state <= 3'h5;
                                 fine = 1'b0;
+                                flag = 1'b0;
                             end
-                            else state <= 3'h3;
-                        end 
-                        else indice = indice + 1; 
-                    end
-                  end  
-                  
-            3'h6: state <= 3'h7;
-                  
-            3'h7: begin
-                if (HASH[255-:10] == 10'h0) begin
-                     OUT = 1; 
-                     if (OUT) begin
-                        addr = indirizzo_nonce; 
-                        rd_n = 1'b0;                                              
-                        NONCE_OUT = bram_data_out[nonce_width-:32]; 
+                            else state <= 3'h2;
+                        end
+                        else indice = indice + 1;
                      end
+                     else begin
+                        flag = 1'b1;
+                        indice = indice + 1;
+                     end           
+                  end
+                  
+            3'h5: state <= 3'h6;
+                  
+            3'h6: begin
+                if (HASH[255-:10] == 10'h0) begin                                      
+                    addr = indirizzo_nonce; 
+                    rd_n = 1'b0;                                              
+                    NONCE_OUT = bram_data_out[nonce_width-:32];                                             
                 end
                 else begin
-                    state <= 3'h2;                                     
+                    state <= 3'h1;                                     
                     rd_n = 1'b0; 
                 end
             end
             
         endcase
+        end
     end
     
 endmodule
